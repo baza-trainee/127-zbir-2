909d4195ef83a436cd20ca23d1ee3a1f3f4b4fb8
Yullia Kravchuk <ljuljek554@gmail.com>
2024-06-14 17:19:11 +0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import About from "./components/About/About.tsx";
+import Footer from "./components/Footer/Footer.tsx";
+import Header from "./components/Header/Header.tsx";
+import Hero from "./components/Hero/Hero.tsx";
+import HowItWorks from "./components/HowItWorks/HowItWorks.tsx";
+import Target from "./components/Target/Target.tsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Target />
+        <About />
+        <HowItWorks />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <p>Розробка Baza Trainee Ukraine 2024 © Всі права захищені</p>;
+};
+
+export default Footer;

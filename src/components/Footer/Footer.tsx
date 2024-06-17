11360f13d28b3e1d7c33151aa02b105f535b6812
Yullia Kravchuk <ljuljek554@gmail.com>
2024-06-17 17:06:29 +0300

    add footer + about

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,14 @@
-const Footer = () => {
-  return <p>Розробка Baza Trainee Ukraine 2024 © Всі права захищені</p>;
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const Footer: React.FC = () => {
+  return (
+    <div className={styles.footer}>
+      <p className={styles.footerText}>
+        Розробка Baza Trainee Ukraine 2024 ©. Всі права захищені.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;

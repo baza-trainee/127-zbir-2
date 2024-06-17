37a33b3eac511d4352b38fc727eb029f8c889a1c
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2024-06-17 09:38:33 +0300

    feat: correct styles, fonts, add Button component v.1, component Hero mobile + laptop

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { FC } from "react";
+import Button from "../Button/Button";
+import "../../styles/index.scss";
+import styles from "./Hero.module.scss";
 
-const Hero = () => {
-  return <div>Hero</div>;
+const Hero: FC = () => {
+  return (
+    <section className={styles.heroWrapper}>
+      <div className="container">
+        <h1 className={styles.headline}>
+          Збір на дрони для 127-мої бригади ТРО ЗСУ{" "}
+        </h1>
+      </div>
+      <div className={styles.buttonWrapper}>
+        <Button variant="support" label="Підтримати" />
+        <Button variant="share" label="Поділитися" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;

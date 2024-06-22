205c4e62e6e7d8b8ce574d17499364aef963ada4
Yullia Kravchuk <ljuljek554@gmail.com>
2024-06-22 14:52:21 +0300

    fix styles

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -3,39 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./Carousel.module.scss";
-
-const imgArr = [
-  {
-    id: 1,
-    src: "/carousel/1.jpg",
-    src2x: "/carousel/1@2x.jpg",
-    altText: "Photo our team 1",
-  },
-  {
-    id: 2,
-    src: "/carousel/2.jpg",
-    src2x: "/carousel/2@2x.jpg",
-    altText: "Photo our team 2",
-  },
-  {
-    id: 3,
-    src: "/carousel/3.jpg",
-    src2x: "/carousel/3@2x.jpg",
-    altText: "Photo our team 3",
-  },
-  {
-    id: 4,
-    src: "/carousel/4.jpg",
-    src2x: "/carousel/4@2x.jpg",
-    altText: "Photo our team 4",
-  },
-  {
-    id: 5,
-    src: "/carousel/5.jpg",
-    src2x: "/carousel/5@2x.jpg",
-    altText: "Photo our team 5",
-  },
-];
+import { dataImg } from "../../assets/dataImg/dataImg";
 
 const Carousel: React.FC = () => {
   const settings = {
@@ -53,7 +21,7 @@ const Carousel: React.FC = () => {
   return (
     <div className={styles.sliderContainer}>
       <Slider {...settings}>
-        {imgArr.map((img) => (
+        {dataImg.map((img) => (
           <picture key={img.id} className={styles.wrapperImages}>
             <source
               srcSet={`${img.src2x} 2x`}
